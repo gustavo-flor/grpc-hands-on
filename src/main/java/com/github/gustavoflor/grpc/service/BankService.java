3ca3f76ac4d92446f062a868b510fab2b413def1
gustavo-flor <gustavo.flor@picpay.com>
2023-10-28 10:58:37 -0300

    :sparkles: Adding channel error improvement

--- src/main/java/com/github/gustavoflor/grpc/service/BankService.java
+++ src/main/java/com/github/gustavoflor/grpc/service/BankService.java
@@ -4,7 +4,9 @@ import com.github.gustavoflor.grpc.observer.DepositStreamObserver;
 import com.github.gustavoflor.grpc.protobuf.*;
 import com.github.gustavoflor.grpc.repository.BalanceRepository;
 import io.grpc.Context;
+import io.grpc.Metadata;
 import io.grpc.Status;
+import io.grpc.protobuf.ProtoUtils;
 import io.grpc.stub.StreamObserver;
 
 import static com.github.gustavoflor.grpc.util.ContextUtil.USER_ID_KEY;
@@ -38,22 +40,56 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase {
         final var amount = request.getAmount();
         final var balance = balanceRepository.findBalanceByAccountNumber(accountNumber);
 
+        if (amount <= 0) {
+            final var metadata = new Metadata();
+            final var errorKey = ProtoUtils.keyForProto(WithdrawError.getDefaultInstance());
+            final var error = WithdrawError.newBuilder()
+                .setBalance(balance)
+                .setStatus(ErrorStatus.AMOUNT_SHOULD_BE_POSITIVE)
+                .build();
+            metadata.put(errorKey, error);
+            final var message = "Invalid input, amount must be a positive value";
+            final var exception = Status.INVALID_ARGUMENT
+                .withDescription(message)
+                .asRuntimeException(metadata);
+            responseObserver.onError(exception);
+            return;
+        }
+
         if (amount < 10 || amount % 10 != 0) {
-            final var message = "Invalid input, amount must be a multiple of 10 and greater than 10";
-            final var status = Status.INVALID_ARGUMENT.withDescription(message);
-            responseObserver.onError(status.asException());
+            final var metadata = new Metadata();
+            final var errorKey = ProtoUtils.keyForProto(WithdrawError.getDefaultInstance());
+            final var error = WithdrawError.newBuilder()
+                .setBalance(balance)
+                .setStatus(ErrorStatus.ONLY_TEN_MULTIPLES)
+                .build();
+            metadata.put(errorKey, error);
+            final var message = "Invalid input, amount must be a multiple of 10";
+            final var exception = Status.INVALID_ARGUMENT
+                .withDescription(message)
+                .asRuntimeException(metadata);
+            responseObserver.onError(exception);
             return;
         }
 
         if (balance.getValue() < amount) {
+            final var metadata = new Metadata();
+            final var errorKey = ProtoUtils.keyForProto(WithdrawError.getDefaultInstance());
+            final var error = WithdrawError.newBuilder()
+                .setBalance(balance)
+                .setStatus(ErrorStatus.INSUFFICIENT_BALANCE)
+                .build();
+            metadata.put(errorKey, error);
             final var message = String.format("Insufficient balance. You have only %s %s.", balance.getValue(), balance.getCurrency());
-            final var status = Status.FAILED_PRECONDITION.withDescription(message);
-            responseObserver.onError(status.asException());
+            final var exception = Status.FAILED_PRECONDITION
+                .withDescription(message)
+                .asRuntimeException(metadata);
+            responseObserver.onError(exception);
             return;
         }
 
         for (int index = 0; index < amount / 10; index++) {
-            setTimeout(3000);
+            setTimeout(1000);
             if (Context.current().isCancelled()) {
                 break;
             }
